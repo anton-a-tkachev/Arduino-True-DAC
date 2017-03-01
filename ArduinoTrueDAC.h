a257d9f14675e2364d9295772bad469aa2eb9da0
Anton Tkachev <anton.a.tkachev@gmail.com>
2017-03-01 12:07:28 +1100

    Update ArduinoTrueDAC.h

--- ArduinoTrueDAC.h
+++ ArduinoTrueDAC.h
@@ -10,6 +10,10 @@
 #ifndef ARDUINOTRUEDAC_h
 #define ARDUINOTRUEDAC_h
 
+void DAC8INIT();
+void DAC8OUT(int SETTING);
+void DAC10INIT();
+void DAC10OUT(int SETTING);
 void DAC12INIT();
 void DAC12OUT(int SETTING);
 
